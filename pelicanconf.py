8c6b4ed1435b1fe7a7f5abbdabd39506ccc2e74e
Siim Juuse <siimjuuse@gmail.com>
2020-11-07 11:56:11 +0200

    lh_first commit

--- pelicanconf.py
+++ pelicanconf.py
@@ -22,11 +22,10 @@ DISPLAY_PAGES_ON_MENU = True
 
 
 # Feed generation is usually not desired when developing
-FEED_ALL_ATOM = None
-CATEGORY_FEED_ATOM = None
-TRANSLATION_FEED_ATOM = None
-AUTHOR_FEED_ATOM = None
-AUTHOR_FEED_RSS = None
+FEED_ALL_ATOM = 'feeds/all.atom.xml'
+FEED_ALL_RSS = 'feeds/all.rss.xml'
+AUTHOR_FEED_RSS = 'feeds/%s.rss.xml'
+RSS_FEED_SUMMARY_ONLY = False
 
 # LINKS FOR WEBSITES
 #LINKS = (('Pelican', 'https://getpelican.com/'),
